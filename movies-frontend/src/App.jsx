068fd993fec91520a44d516fb6a589eeb66ad6bb
Henry <henry.a.s.arrovin@gmail.com>
2024-06-22 22:05:33 +0530

    Created the basic frontend design - But errors can be found in the review section

--- movies-frontend/src/App.jsx
+++ movies-frontend/src/App.jsx
@@ -4,10 +4,16 @@ import { useEffect, useState } from 'react';
 import Layout from './components/Layout';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/home/Home';
+import Header from './components/header/Header';
+import Trailer from './components/trailer/Trailer';
+import Reviews from './components/reviews/Reviews';
+import NotFound from './components/notFound/NotFound';
 
 function App() {
 
   const [movies, setMovies] = useState([]);
+  const [movie, setMovie] = useState();
+  const [reviews, setReviews] = useState([]);
 
   const getMovies = async () => {
     try {
@@ -19,15 +25,30 @@ function App() {
     }
   }
 
+  const getMovieData = async (movieId) => {
+    try {
+        const response = await api.get(`/movies/${movieId}`);
+        const singleMovie = response.data;
+        setMovie(singleMovie);
+        setReviews(singleMovie.reviews);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   useEffect(() => {
     getMovies();
   }, []);
   
   return (
     <div>
+      <Header />
       <Routes>
         <Route path='/' element={<Layout />}>
           <Route path='/' element={<Home movies={movies} />} />
+          <Route path='/Trailer/:ytTrailerId' element={<Trailer />}/>
+          <Route path="/Reviews/:movieId" element ={<Reviews getMovieData = {getMovieData} movie={movie} reviews ={reviews} setReviews = {setReviews} />} />
+          <Route path="*" element = {<NotFound/>}></Route>
         </Route>
       </Routes>
     </div>
